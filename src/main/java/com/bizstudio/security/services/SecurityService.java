b4f79a0f110ba997ee8a0f7ac7f4fbbf5ed2733e
bnnsz <bnnsz384@gmail.com>
2019-05-16 11:08:47 +0100

    Fixed session not found exception

--- src/main/java/com/bizstudio/security/services/SecurityService.java
+++ src/main/java/com/bizstudio/security/services/SecurityService.java
@@ -203,7 +203,7 @@ public class SecurityService extends AuthorizingRealm implements
     public SimpleSession readSession(Serializable sessionId) throws UnknownSessionException {
         SessionEntity findById = sessionRepository.findById((Long) sessionId);
         if (findById == null || findById == null) {
-            return null;
+            throw new UnknownSessionException("Session with id "+findById+" was not found");
         }
         return findById.toSession();
     }
